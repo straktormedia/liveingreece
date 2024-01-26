f038371ca5a88c3161aba55a35d3a273593a61d3
vasilisCM <vpafiliaris@conceptmaniax.com>
2024-01-26 23:45:13 +0200

    better transform origin and animation

--- src/js/logic/customCursor.js
+++ src/js/logic/customCursor.js
@@ -1,20 +1,23 @@
 // Custom Cursor
 const customCursor = (cursor, duration) => {
   const trackCursorPosition = (e) => {
-    gsap.to(cursor, {
-      top: `${e.pageY}`,
-      left: `${e.pageX}`,
-      duration: duration,
-    });
+    // gsap.to(cursor, {
+    //   top: `${e.pageY}`,
+    //   left: `${e.pageX}`,
+    //   duration: duration,
+    // });
 
-    const arrow = document.querySelector(".cursor__arrow");
+    cursor.style.top = e.pageY + "px";
+    cursor.style.left = e.pageX + "px";
+
+    // const arrow = document.querySelector(".cursor__arrow");
     // gsap.set(arrow, {
     //   transformOrigin: 'center right',
     // });
 
-    const cursorY = e.clientY;
-    const rotation = (cursorY / window.innerHeight) * 90 - 45;
-    const constrainedRotation = Math.min(45, Math.max(-45, rotation));
+    // const cursorY = e.clientY;
+    // const rotation = (cursorY / window.innerHeight) * 90 - 45;
+    // const constrainedRotation = Math.min(45, Math.max(-45, rotation));
 
     // gsap.to(arrow, {
     //   rotate: -constrainedRotation,
